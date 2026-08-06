352b654f85759ad449e43c67c9549c8c71d457ce
Darren <vicenciojohndarren@gmail.com>
2026-08-06 23:19:08 +0800

    feat: initialize MERN starter kit foundation

---
+++ README.md
@@ -0,0 +1,13 @@
+# MERN Starter Kit
+
+A production-ready MERN stack starter template built with:
+
+- React + Vite
+- Express.js
+- MongoDB
+- JWT Authentication
+- Tailwind CSS
+- Axios
+- Reusable Components
+
+Created by Darren Vicencio
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
